8c59d29acd9bc079d517c7199991a262bae90d9f
rose-charlotte <charlotte@anros.fr>
2024-04-25 16:16:53 +0200

    mise en place de fontAwesome

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,12 @@
-import argentBankLogo from "../../../public/argentBankLogo.png";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+import argentBankLogo from "/argentBankLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function Header() {
     return (
         <nav>
             <img src={argentBankLogo} />
+            <FontAwesomeIcon icon={faUserCircle} />
             <a>Sign In</a>
         </nav>
     );
