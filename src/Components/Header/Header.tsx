9c71018746efc7de752e7fe1f0d4b8a31b98b4f4
rose-charlotte <charlotte@anros.fr>
2024-04-29 10:26:50 +0200

    mise en place du Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
 import argentBankLogo from "/argentBankLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import style from "./Header.module.scss";
 
 export function Header() {
     return (
-        <nav>
-            <img src={argentBankLogo} />
-            <FontAwesomeIcon icon={faUserCircle} />
-            <a>Sign In</a>
+        <nav className={style.mainNav}>
+            <Link className={style.mainNavLogo} to="/">
+                <img className={style.mainNavLogoImg} src={argentBankLogo} alt="logo argent bank" />
+            </Link>
+            <Link className={style.mainNavItem} to="/signIn">
+                <FontAwesomeIcon icon={faUserCircle} />
+                <span>Sign In</span>
+            </Link>
         </nav>
     );
 }
