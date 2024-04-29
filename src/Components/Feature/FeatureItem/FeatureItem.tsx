a4710c61053d0f33f9fdacd9352e924dfaeec546
rose-charlotte <charlotte@anros.fr>
2024-04-29 12:01:03 +0200

    feature éise en dynamique

--- src/Components/Feature/FeatureItem/FeatureItem.tsx
+++ src/Components/Feature/FeatureItem/FeatureItem.tsx
@@ -1,14 +1,17 @@
 import style from "./FeatureItem.module.scss";
 
-export function FeatureItem() {
+export function FeatureItem(props: FeatureItemProps) {
     return (
         <div className={style.feature}>
-            <img className={style.icon} src="icon-chat.png" alt="logo" />
-            <h1 className={style.title}>You are our #1 priority</h1>
-            <p>
-                Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in
-                less than 5 minutes.
-            </p>
+            <img className={style.icon} src={props.src} alt="logo" />
+            <h1 className={style.title}>{props.title}</h1>
+            <p>{props.text}</p>
         </div>
     );
+}
+
+export interface FeatureItemProps {
+    src: string;
+    title: string;
+    text: string;
 }
--- src/Components/Feature/FeatureList/FeatureList.tsx
+++ src/Components/Feature/FeatureList/FeatureList.tsx
@@ -4,9 +4,21 @@ import style from "./FeatureList.module.scss";
 export function FeaturesList() {
     return (
         <section className={style.feature}>
-            <FeatureItem />
-            <FeatureItem />
-            <FeatureItem />
+            <FeatureItem
+                src="icon-chat.png"
+                title="You are our #1 priority"
+                text="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+            />
+            <FeatureItem
+                src="icon-money.png"
+                title="More savings means higher rates"
+                text="The more you save with us, the higher your interest rate will be!"
+            />
+            <FeatureItem
+                src="icon-security.png"
+                title="Security you can trust"
+                text="We use top of the line encryption to make sure your data and money is always safe."
+            />
         </section>
     );
 }
