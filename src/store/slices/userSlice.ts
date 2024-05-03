727fdc6fc85388f0c834e584beb165e9954445e5
rose-charlotte <charlotte@anros.fr>
2024-05-03 11:24:40 +0200

    creation store et userSlice

---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    token: undefined,
+    user: undefined,
+};
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setToken: (state, action) => {
+            state.token = action.payload;
+        },
+
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+    },
+});
+
+export const { setToken, setUser } = userSlice.actions;
+export default userSlice.reducer;
