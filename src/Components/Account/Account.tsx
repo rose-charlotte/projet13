4d4172d2516aae94f906f1339ddb1dd52b7846ad
rose-charlotte <charlotte@anros.fr>
2024-05-06 11:20:43 +0200

    rectification + ajout bankAccounts

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -1,16 +1,22 @@
 import style from "./Account.module.scss";
 
-export function Account() {
+export function Account(props: AccountProps) {
     return (
         <section className={style.account}>
             <div className={style.accountContentWrapper}>
-                <h1 className={style.accountTitle}>Argent Bank Checking (x8349)</h1>
-                <p className={style.accountAmount}>$2,082.79</p>
-                <p className={style.accountAmountDescription}>Available Balance</p>
+                <h1 className={style.accountTitle}>{props.title}</h1>
+                <p className={style.accountAmount}>{props.amount}</p>
+                <p className={style.accountAmountDescription}>{props.description}</p>
             </div>
             <div className={style.accountContentWrapperCta}>
                 <button className={style.transactionBtn}>View transaction</button>
             </div>
         </section>
     );
+}
+
+export interface AccountProps {
+    title: string;
+    amount: string;
+    description: string;
 }
--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -4,6 +4,24 @@ import { UserHeader } from "../../Components/UserHeader/UserHeader";
 import style from "./UserPage.module.scss";
 import { selectToken, setUser } from "../../store/slices/userSlice";
 
+const bankAccounts = [
+    {
+        title: "Argent Bank Checking (x8349)",
+        amount: "$2,082.79",
+        description: "Available Balance",
+    },
+    {
+        title: "Argent Bank Savings (x6712)",
+        amount: "$10,928.42",
+        description: "Available Balance",
+    },
+    {
+        title: "Argent Bank Credit Card (x8349)",
+        amount: "$184.30",
+        description: "Current Balance",
+    },
+];
+
 export function UserPage() {
     const dispatch = useDispatch();
 
@@ -25,7 +43,9 @@ export function UserPage() {
         <main className={style.main}>
             <UserHeader />
             <h2 className={style.srOnly}>Accounts</h2>
-            <Account />
+            {bankAccounts.map(account => (
+                <Account title={account.title} amount={account.amount} description={account.description} />
+            ))}
         </main>
     );
 }
