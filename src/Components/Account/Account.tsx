a634d84cffd3a273a8aa00d51b7eb760b01253cc
rose-charlotte <charlotte@anros.fr>
2024-05-22 11:09:13 +0200

    modification btn

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -1,22 +1,30 @@
+import { Link } from "react-router-dom";
 import style from "./Account.module.scss";
 
 export function Account(props: AccountProps) {
     return (
         <section className={style.account}>
             <div className={style.accountContentWrapper}>
-                <h1 className={style.accountTitle}>{props.title}</h1>
-                <p className={style.accountAmount}>{props.amount}</p>
-                <p className={style.accountAmountDescription}>{props.description}</p>
+                <h1 className={style.accountTitle}>
+                    Argent BanK {props.type} {props.accountId}
+                </h1>
+                <p className={style.accountAmount}>{props.currency}</p>
+                <p className={style.accountAmountDescription}>{props.balanceType}</p>
             </div>
             <div className={style.accountContentWrapperCta}>
-                <button className={style.transactionBtn}>View transactions</button>
+                <button className={style.transactionBtn}>
+                    <Link className={style.link} to="/profile/bankAccount/transactions">
+                        View transactions
+                    </Link>
+                </button>
             </div>
         </section>
     );
 }
 
 export interface AccountProps {
-    title: string;
-    amount: string;
-    description: string;
+    type: string;
+    accountId: string;
+    currency: string;
+    balanceType: string;
 }
